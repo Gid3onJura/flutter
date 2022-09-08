e12b08c4dc7eac851f32ee0ed1b7b4ed50e6128a
Gid3onJura <homebasekorlash@googlemail.com>
2022-09-08 23:28:22 +0200

    async funtions

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,10 +8,32 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    // simulate network request for a username
+    await Future.delayed(Duration(seconds: 3), () {
+      print('yo');
+    });
+
+    print('statement');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('choose location screen'),
-    );
+    print('build function ran');
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Choose a loaction'),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: Text(''));
   }
 }
