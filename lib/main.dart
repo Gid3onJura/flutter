4a358fb2c316df5628f98234230aeef01334b47a
Gid3onJura <homebasekorlash@googlemail.com>
2022-09-08 22:37:47 +0200

    create screens widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,8 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:playground/pages/home.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: QuoteList(),
+    home: Home(),
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({super.key});
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote('Rechne immer mit dem Schlimmsten', 'Oscar Wilde'),
-    Quote('Lebe dein Leben', 'James Dean'),
-    Quote('Lorum ipsum Lorum ipsum Lorum ipsum Lorum ipsum', 'Lorem ipsum'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: const Text('Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes
-            .map((quote) => QuoteCard(
-                quote: quote,
-                delete: () {
-                  setState(() {
-                    quotes.remove(quote);
-                  });
-                }))
-            .toList(),
-      ),
-    );
-  }
 }
