fce743d4f9da46ef50fe877cb7bb57e4e5707727
Gid3onJura <homebasekorlash@googlemail.com>
2022-09-11 21:08:37 +0200

    create world time class, connect to api's

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,21 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    // simulate network request for a username
-    await Future.delayed(Duration(seconds: 3), () {
-      print('yo');
-    });
-
-    print('statement');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build function ran');
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:playground/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -7,7 +12,28 @@ class Loading extends StatefulWidget {
   State<Loading> createState() => _LoadingState();
 }
 
+class LocationData {
+  int flag;
+  String url;
+
+  LocationData({required this.flag, required this.url});
+}
+
 class _LoadingState extends State<Loading> {
+  Map locationData = {'berlin': LocationData(flag: 276, url: 'Berlin/Europe')};
+
+  void setUpWorldTime() {
+    WorldTime instance = WorldTime(
+        location: 'Berlin',
+        flag: locationData['berlin'].flag,
+        url: locationData['berlin'].url);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(body: Text('loading screen...'));
